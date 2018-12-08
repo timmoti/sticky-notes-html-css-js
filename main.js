d1db61e47467bcd726f07274bfad14052dadaa0a
TimothyTan <timmoti@gmail.com>
2018-12-08 23:59:46 +0800

    Added save functionality

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const addNoteButton = document.querySelector('#btnNew');
 
 addNoteButton.addEventListener('click', addNewNote);
+const notes = document.querySelector('#notes');
 
 function addNewNote(title, content) {
-  const notes = document.querySelector('#notes');
-  const li = document.createElement('li');
-  li.setAttribute('class', 'colour');
+  const newNote = document.createElement('li');
 
   const noteTitle = document.createElement('input');
   Object.assign(noteTitle, {
@@ -23,17 +22,27 @@ function addNewNote(title, content) {
     src: 'images/close.png'
   });
 
-  li.appendChild(noteTitle);
-  li.appendChild(image);
-  li.appendChild(noteContent);
-  notes.appendChild(li);
+  newNote.appendChild(noteTitle);
+  newNote.appendChild(image);
+  newNote.appendChild(noteContent);
+  notes.appendChild(newNote);
 
-  let newNote = notes.lastElementChild;
-  newNote.querySelector('img').addEventListener('click', function() {
-    newNote.parentNode.removeChild(newNote);
+  let newestNote = notes.lastElementChild;
+  newestNote.querySelector('img').addEventListener('click', function() {
+    newestNote.parentNode.removeChild(newestNote);
   });
 
-  addNoteEvent(newNote);
+  addNoteEvent(newestNote);
+
+  if (title && !MouseEvent) {
+    const newNoteTitle = newestNote.querySelector('.note-title');
+    newNoteTitle.value = title;
+  }
+
+  if (content) {
+    const newNoteContent = newestNote.querySelector('.note-content');
+    newNoteContent.textContent = content;
+  }
 }
 
 function addNoteEvent(noteElement) {
@@ -46,4 +55,37 @@ function addNoteEvent(noteElement) {
     const img = noteElement.querySelector('img');
     img.setAttribute('class', 'hide');
   });
+}
+
+let count = 0;
+
+const storedNotes = localStorage.getItem('notes');
+if (storedNotes) {
+  const notesArray = JSON.parse(storedNotes);
+  count = notesArray.length;
+
+  for (let i = 0; i < count; i++) {
+    let storedNote = notesArray[i];
+    addNewNote(storedNote.title, storedNote.content);
+  }
+}
+
+const saveNotesButton = document.querySelector('#btnSave');
+saveNotesButton.addEventListener('click', saveNotes);
+
+function saveNotes() {
+  const notesArray = new Array();
+  const listArray = Array.from(document.querySelectorAll('li'));
+  listArray.forEach(element => {
+    const title = element.querySelector('.note-title');
+    const content = element.querySelector('.note-content');
+    notesArray.push({
+      title: title.value,
+      content: content.value
+    });
+  });
+
+  const jsonString = JSON.stringify(notesArray);
+  localStorage.setItem('notes', jsonString);
+  alert('Notes saved');
 }
